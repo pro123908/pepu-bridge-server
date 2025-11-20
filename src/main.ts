fbdabd806e15177b118f314feaac8c862212308d
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2025-11-20 14:10:29 +0100

    fix: allow all cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Allows ALL origins, methods, headers
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  }); // Allows ALL origins, methods, headers
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
